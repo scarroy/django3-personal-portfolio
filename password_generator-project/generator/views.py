db099f7b3b4c5ad646bd83178a29ec0757ddb118
Steven Arroyo <scarroyo@lanl.gov>
2022-04-07 08:39:12 -0600

    initial commit

---
+++ password_generator-project/generator/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+
+#sca
+from django.http import HttpResponse
+import random 
+
+# Create your views here.
+#sca
+def home(request):
+    return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def password(request):
+    
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    if request.GET.get('special'):
+        characters.extend('!@#$%^&*()_-+=')
+    if request.GET.get('numbers'):
+        characters.extend('1234567890')
+
+    length = int(request.GET.get('length', 12))
+
+    thepassword = ""
+
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password':thepassword})
