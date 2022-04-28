0c608d3cfd231ebfff6dbfb713861446d0a9e80f
Steven Arroyo <steven.c.arroyo@gmail.com>
2022-04-28 17:23:37 +0000

    removed old data

--- personal_porfolio-project/personal_porfolio/settings.py
+++ personal_porfolio-project/personal_porfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f+2s75vy61=ko+c3uhbyf6q-z6e0#v(hn&18##=!e++@h4q8=6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['scarroyo.pythonanywhere.com']
 
 
 # Application definition
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
